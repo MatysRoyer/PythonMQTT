c9eb95431c1e708fee45ba1c2d00968a22dd48ff
MatysRoyer <2141356@cegepba.qc.ca>
2023-09-06 13:33:03 -0400

    modification de Mqtt client

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import dht11
 import paho.mqtt.client as mqtt
 
 broker = '10.4.1.42'
-client = mqtt.Client("Matys")
+client = mqtt.Client("Matys", clean_session=False)
 
 
-def on_connect(clientTest, userdata, flags, rc):
-    print("I am connected " + str(rc))
-
+def on_connect(client, userdata, flags, rc):
+    print("I am connected " + str(rc) + str(flags))
+    client.subscribe("SMILE", qos=2)
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
@@ -22,5 +22,4 @@ def on_message(client, userdata, msg):
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(broker, 1883)
-client.subscribe("SMILE")
 client.loop_forever()
