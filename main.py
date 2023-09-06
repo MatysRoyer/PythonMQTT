fcf810514af68918321585bacc24a3fad8d81ae7
MatysRoyer <2141356@cegepba.qc.ca>
2023-09-06 13:08:13 -0400

    ajout du code pour Mqtt

--- main.py
+++ main.py
@@ -5,18 +5,22 @@
 
 import RPi.GPIO as GPIO
 import dht11
+import paho.mqtt.client as mqtt
 
-# initialize GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
+broker = '10.4.1.42'
+client = mqtt.Client("Matys")
 
-# read data using pin 14
-instance = dht11.DHT11(pin = 11)
-result = instance.read()
 
-if result.is_valid():
-    print("Temperature: %-3.1f C" % result.temperature)
-    print("Humidity: %-3.1f %%" % result.humidity)
-else:
-    print("Error: %d" % result.error_code)
+def on_connect(clientTest, userdata, flags, rc):
+    print("I am connected " + str(rc))
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker, 1883)
+client.subscribe("SMILE")
+client.loop_forever()
